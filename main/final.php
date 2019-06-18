f31ee39c456f03892dcc52847c180b75e170d93b
himanshigupta10 <47886389+himanshigupta10@users.noreply.github.com>
2019-06-19 02:17:03 +0530

    Update final.php

--- main/final.php
+++ main/final.php
@@ -1,63 +1,10 @@
-<?php include ('database.php'); ?>
-<?php 
-if(isset($_POST['submit'])){
+         
 
-$question_number = $_POST['question_number'];
-$question_number = $_POST['question_text'];
-$correct_choice = $_POST['correct_choice'];
-
-$choices = array();
-$choices[1] = $_POST['choice1'];
-$choices[2] = $_POST['choice2'];
-$choices[3] = $_POST['choice3'];
-$choices[4] = $_POST['choice4'];
-$choices[5] = $_POST['choice5'];
-
-
-$query = "INSERT INTO `questions`(question_number, text)
-                                VALUES('$question_number','question_text')";
-
-$insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
-
-if($insert_row){
-       foreach($choices as $choice => $value){
-       	     if($value != ''){
-       	     	if($correct_choice == $choice){
-       	     		$is_correct = 1;
-
-       	     	} else {
-       	     		$is_correct = 0;
-
-       	     	}
-       	     	$query = "INSERT INTO `choices` (question_number, is_correct, text)
-       	     	VALUES ('question_number','$is_correct','$value')";
-       	     	$insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
-
-       	     	if($insert_row){
-       	     		continue; 
-       	     	} else {
-       	     		die('Error : ('.$mysqli->errno . ') '. $mysqli->error);
-       	     	}
-
-       	     }
-       }
- $msg = 'Question has been added';
-  }
-
-}
-
-$query = "SELECT *FROM `questions`";
-$results = $mysqli->query($query) or die($mysqli->error.__LINE__);
-$total = $results->num_rows;
-$next = $total+1;
+<?php session_start(); ?>
+        <!DOCTYPE html>
 
 
 
-
-?>
-
-
-<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8" />
@@ -72,54 +19,14 @@ $next = $total+1;
  	</header>
  	<main>
  		<div class="container">
- 		<h2>Add A Question</h2>
-<?php 
-if(isset($msg)){
-	echo '<p'.$msg.'</p>';
-}
-?>
+ 		<h2> You're done!</h2>
+ 		<p>Congrats you have completed the test</p>
+ 		<p>Final score <?php echo $_SESSION['score']; ?></p>
+        <a href="question.php?n=1" class="start"> Try Again</a> 		
 
- 		<form method="post" action="add.php">
- 			<p>
- 				<label>Question Number</label>
- 				<input type="number" value="<?php echo $next; ?>" name="question_number" />
- 			</p>
 
-<p>
- 				<label>Question Text</label>
- 				<input type="text" name="question_text" />
- 			</p>
- 			
- 			<p>
- 				<label>Choice #1:</label>
- 				<input type="text" name="choice1" />
- 			</p>
 
- 			<p>
- 				<label>Choice #2:</label>
- 				<input type="text" name="choice2" />
-    			</p>
- 			<p>
- 				<label>Choice #3:</label>
- 				<input type="text" name="choice3" />
- 			</p>
- 			<p>
- 				<label>Choice #4:</label>
- 				<input type="text" name="choice4" />
- 			</p>
- 			<p>
- 				<label>Choice #5:</label>
- 				<input type="text" name="choice5" />
- 			</p>
- 			<p>
- 				<label>Correct Choice Number:</label>
- 				<input type="number" name="correct_choice" />
- 			</p>
- 			<p>
- 				<input type="submit" name="submit" value="submit" />  
- 			</p>
- 		</form>
- 		</div>
+ 			</div>
  
  			
  		</main>
